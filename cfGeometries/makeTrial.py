fa8709179a48917e444435eab6d71a2334b781b0
Martin Juckes <martin.juckes@stfc.ac.uk>
2017-07-13 13:22:36 +0100

    Added code to read and plot the regions

--- cfGeometries/makeTrial.py
+++ cfGeometries/makeTrial.py
@@ -1,6 +1,15 @@
 import xlrd, os
 import collections
 import netCDF4,numpy
+import shapely
+import shapely.affinity
+from shapely.geometry import Polygon
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+from mpl_toolkits.basemap import Basemap, shiftgrid, addcyclic
+import numpy as np
 
 class regionsIn(object):
   nt_rec = collections.namedtuple( 'rec', ['title','label','nsres','mask','lon','lat'] )
@@ -95,3 +104,154 @@ class nc1(object):
     self.nc.close()
      
 
+class ncin(object):
+  nt__rec = collections.namedtuple( 'rec', ['title','label','points'] )
+  nt__ip = collections.namedtuple( 'ip', ['val1','title','val2'] )
+  def __init__(self,fn='regionsTrial.nc'):
+    self.nc = netCDF4.Dataset( fn, 'r')
+    self.vars = {}
+    self.attr = {}
+    self.dimensions = {}
+    for k in self.nc.dimensions.keys():
+      self.dimensions[k] = len( self.nc.dimensions[k] )
+
+    for k in self.nc.variables.keys():
+      ee = {}
+      for a in self.nc.variables[k].ncattrs():
+        ee[a] = self.nc.variables[k].getncattr( a )
+      self.vars[k] = {'data':self.nc.variables[k][:], 'attr':ee}
+    self.close()
+
+    self.parseGeom()
+    self.shapely()
+
+  def shapely(self):
+    self.polys = []
+    self.mpolys = []
+    self.ipolys = []
+    domain = shapely.geometry.box(-180.0,-90.,180.,90.)
+    
+    colsv = range( len( self.ll ) )
+    numpy.random.shuffle( colsv )
+    k = 0
+    for r in self.ll:
+      sp = Polygon(r.points)
+      spc = list(sp.exterior.coords)
+      lons = [x[0] for x in spc]
+      if max(lons) > 180.:
+        spa = domain.intersection( sp )
+        sp2 = shapely.affinity.translate(sp,xoff=-360)
+        spb = domain.intersection( sp2 )
+        spc = list(sp2.exterior.coords)
+
+        self.polys.append( spa )
+        self.ipolys.append( self.nt__ip( colsv[k], r.title, 'a' ) )
+        mp = matplotlib.patches.Polygon(list(spa.exterior.coords))
+        mp.set_label( r.label )
+        mp.set_facecolor( matplotlib.cm.jet(k*6) )
+        self.mpolys.append( mp )
+
+        self.polys.append( spb )
+        self.ipolys.append( self.nt__ip( colsv[k], r.title, 'b' ) )
+        mp = matplotlib.patches.Polygon(list(spb.exterior.coords))
+        mp.set_label( r.label + "'" )
+        mp.set_facecolor( matplotlib.cm.jet(k*6) )
+        mp.set_edgecolor( matplotlib.cm.jet(k*6) )
+        self.mpolys.append( mp )
+      else:
+        self.polys.append( sp )
+        self.ipolys.append( self.nt__ip( colsv[k], r.title, 'a' ) )
+ 
+        mp = matplotlib.patches.Polygon(spc)
+        mp.set_label( r.label )
+        mp.set_facecolor( matplotlib.cm.jet(k*6) )
+        self.mpolys.append( mp )
+      k += 1
+
+    fig = plt.figure(figsize =(8,6))
+    ax = plt.gca()
+    plt.tick_params(direction='out', which='both')
+
+#set up map
+    mymap = Basemap(projection='cyl',llcrnrlon=-180, urcrnrlon=180, \
+                llcrnrlat=-90, urcrnrlat=90, \
+                lon_0=0, lat_0=0, resolution='c')
+
+#colour land, sea and lakes
+    mymap.fillcontinents(color='coral',lake_color='aqua')
+    mymap.drawmapboundary(fill_color='aqua')
+    p = PatchCollection(self.mpolys, cmap=matplotlib.cm.jet, alpha=0.5, zorder=2)
+
+    colors = [x.val1 for x in self.ipolys]
+    p.set_array(np.array(colors))
+
+    fp = matplotlib.font_manager.FontProperties( size=8 )
+    ax.add_collection(p)
+    for k in range(len(self.polys)):
+      xc = self.polys[k].centroid.x
+      yc = self.polys[k].centroid.y
+      plt.text(xc,yc, self.mpolys[k].get_label(), horizontalalignment='center', fontproperties=fp, va='center')
+
+    plt.show()
+
+  def parseGeom(self):
+    node_count_name = self.vars['geometry_container']['attr']['node_count']
+    node_count = self.vars[node_count_name]['data']
+    assert len( node_count ) == self.dimensions['instance'], '%s != %s' % ( len( node_count ), self.dimensions['instance'] )
+    assert sum( node_count ) == self.dimensions['node']
+    self.ll = []
+    ni = self.dimensions['instance']
+    i0 = 0
+    lon = self.vars['lon']['data'].tolist()
+    lat = self.vars['lat']['data'].tolist()
+    for k in range( ni):
+      i1 = i0 + node_count[k]
+      points = []
+      for i in range(i0,i1):
+        points.append( [lon[i],lat[i]] )
+
+      self.ll.append( self.nt__rec(
+           netCDF4.chartostring(self.vars['region_title']['data'][k]).tolist(),
+           netCDF4.chartostring(self.vars['region_names']['data'][k]).tolist(),
+           points ) )
+      i0 = i1
+##
+## need to break up, using node_count
+##
+  def close(self):
+    self.nc.close()
+
+class pshp(object):
+  def __init__(self,mshp):
+    plt.figure(figsize=(11, 8))
+    plt.tick_params(direction='out', which='both')
+
+#set up map
+    mymap = Basemap(projection='cyl',llcrnrlon=-180, urcrnrlon=180, \
+                llcrnrlat=-90, urcrnrlat=90, \
+                lon_0=0, lat_0=0, resolution='c')
+
+#colour land, sea and lakes
+    mymap.fillcontinents(color='coral',lake_color='aqua')
+    mymap.drawmapboundary(fill_color='aqua')
+
+##plt.text(43, 55, 'Moscow', horizontalalignment='left', verticalalignment='center')
+##plt.plot(37, 55, marker='o', color="red")
+
+    for k in range(len(mshp.ll)):
+      xpts=[p[0] for p in mshp.ll[k].points]
+      ypts=[p[1] for p in mshp.ll[k].points]
+      xpts.append(xpts[0])
+      ypts.append(ypts[0])
+      plt.plot(xpts,ypts, linewidth=3.0, color='blue', alpha=0.5, )
+      xc = mshp.polys[k].centroid.x
+      yc = mshp.polys[k].centroid.y
+      plt.text(xc,yc, mshp.ll[k].label, horizontalalignment='center')
+
+#axes
+    plt.xticks(np.arange(-180, 210, 60), ['180', '120W', '60W', '0', '60E', '120E', '180'])
+    plt.yticks(np.arange(-90, 120, 30), ['90S', '60S', '30S', '0', '30N', '60N', '90N'])
+#coastlines and title
+    mymap.drawcoastlines()
+
+    plt.savefig('ex28.png')
